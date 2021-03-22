3cc19351d6d8dcfa6d970bd4a168e428c3a5f51a
Anthony Ho <AnthonyHo0728@gmail.com>
2021-03-21 21:27:53 -0700

    Activities with backgrounds

--- app/src/main/java/com/example/pawspetntrack/Home.java
+++ app/src/main/java/com/example/pawspetntrack/Home.java
@@ -1,7 +1,10 @@
 package com.example.pawspetntrack;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -18,6 +21,27 @@ public class Home extends AppCompatActivity {
         SimpleDateFormat day = new SimpleDateFormat("EEEE");
         String currentDate = java.text.DateFormat.getDateInstance().format(new Date());
         date.setText( day.format(now) + ",\n" + currentDate);
-
     }
+
+    public void onMark(View v) {
+        Intent intent = new Intent(this, Mark.class);
+        startActivity(intent);
+    }
+
+    public void onNewPackage(View v) {
+        Intent intent = new Intent(this, NewPackage.class);
+        startActivity(intent);
+    }
+
+    public void onAdd(View v) {
+        Intent intent = new Intent(this, AddDog.class);
+        startActivity(intent);
+    }
+
+    public void onPackages(View v) {
+        Intent intent = new Intent(this, Packages.class);
+        startActivity(intent);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/pawspetntrack/NewPackage.java
@@ -0,0 +1,14 @@
+package com.example.pawspetntrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewPackage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_package);
+    }
+}
