50062954661cc82a2e69d8d76f3a6f3d08bba240
Anthony Ho <AnthonyHo0728@gmail.com>
2021-03-21 19:59:59 -0700

    Navigation + Date

--- app/src/main/java/com/example/pawspetntrack/Home.java
+++ app/src/main/java/com/example/pawspetntrack/Home.java
@@ -1,14 +1,23 @@
 package com.example.pawspetntrack;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Home extends AppCompatActivity {
-
+    TextView date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        date = findViewById(R.id.Date);
+        Date now = new Date();
+        SimpleDateFormat day = new SimpleDateFormat("EEEE");
+        String currentDate = java.text.DateFormat.getDateInstance().format(new Date());
+        date.setText( day.format(now) + ",\n" + currentDate);
+
     }
 }
--- app/src/main/java/com/example/pawspetntrack/MainActivity.java
+++ app/src/main/java/com/example/pawspetntrack/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.pawspetntrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = findViewById(R.id.loginButton);
     }
+
+
+    // Login function
+    public void onLogin(View v) {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
+
 }
